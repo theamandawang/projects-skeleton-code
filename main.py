836f79cb9e988317adb122883ab96ec72755c275
Amanda Wang <52474776+theamandawang@users.noreply.github.com>
2022-05-19 19:07:15 -0700

    oops merge conflicts

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
 import os
-<<<<<<< HEAD
-import numpy as np
-=======
->>>>>>> main
 import torch
 import constants
 from data.StartingDataset import StartingDataset
@@ -29,21 +25,7 @@ def main():
 # Initalize dataset and model. Then train the model!
     train_dataset = StartingDataset(True)
     val_dataset = StartingDataset(False)
-<<<<<<< HEAD
-    samp1 = np.random.choice(len(train_dataset), size=int(0.1*len(train_dataset)), replace=False)
-    samp2 = np.random.choice(len(val_dataset), size=int(0.1*len(val_dataset)), replace=False)
-
-    trainset_1 = torch.utils.data.Subset(train_dataset, samp1)
-    valset_1 = torch.utils.data.Subset(train_dataset, samp2)
-    # trainset_2 = torch.utils.data.Subset(train_dataset, odds)
-
-    # trainloader_1 = torch.utils.data.DataLoader(trainset_1, batch_size=4, shuffle=True, num_workers=2)
-    # valloader_1 = torch.utils.data.DataLoader(valset_1, batch_size=4,
-    #                                      shuffle=True, num_workers=2)
-    model = StartingNetwork()
-=======
     model = Model_b()
->>>>>>> main
     starting_train(
         train_dataset=trainset_1,
         val_dataset=valset_1,
