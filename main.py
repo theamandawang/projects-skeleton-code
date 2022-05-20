ff3328b68c048e31ade032635f07703ffe13b467
Amanda Wang <52474776+theamandawang@users.noreply.github.com>
2022-05-19 19:11:09 -0700

    fix typos

--- main.py
+++ main.py
@@ -27,8 +27,8 @@ def main():
     val_dataset = StartingDataset(False)
     model = Model_b()
     starting_train(
-        train_dataset=trainset_1,
-        val_dataset=valset_1,
+        train_dataset=train_dataset,
+        val_dataset=val_dataset,
         model=model,
         hyperparameters=hyperparameters,
         n_eval=constants.N_EVAL,
